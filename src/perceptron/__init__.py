4180d914ad61011e509062c3c1dec94752b30cdc
Max Zheng <max@perceptron.inc>
2026-03-05 13:25:00 -0800

    Sync version and fix release (#52)

--- src/perceptron/__init__.py
+++ src/perceptron/__init__.py
@@ -13,7 +13,7 @@ network access. Transport and streaming are added in later phases.
 
 import importlib
 
-__version__ = "0.1.4"
+__version__ = "0.2.1"
 
 from .annotations import annotate_image
 from .client import (
