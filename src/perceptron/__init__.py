94ae44883ff75d1aacc5880793129a100181e8f3
Greg Priessnitz <greg@perceptron.inc>
2026-09-22 23:53:55 -0700

    Bump version to 0.4.0

--- src/perceptron/__init__.py
+++ src/perceptron/__init__.py
@@ -13,7 +13,7 @@ network access. Transport and streaming are added in later phases.
 
 import importlib
 
-__version__ = "0.3.5"
+__version__ = "0.4.0"
 
 from .annotations import annotate_image
 from .client import (
